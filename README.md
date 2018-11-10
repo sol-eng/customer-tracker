27e272de98e72df868fb034bdad24bdf91882144
Nathan Stephens <nathan@rstudio.com>
2018-11-10 19:45:59 +0000

    updating all outputs for consistency

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 These dashboards and reports track key performance metrics for customers by week. Users can interact with these reports in order to pull the precise data they are looking for. Read more about the customer tracker on [R Views](https://rviews.rstudio.com/2018/05/16/replacing-excel-reports-with-r-markdown-and-shiny/) or or view the code on [Github](https://github.com/sol-eng/customer-tracker).
 
-<center><a href="http://colorado.rstudio.com:3939/tracker/README.html"><img src="images/tracker-rmd.png" width="500"/></a></center>
+<center><a href="http://colorado.rstudio.com:3939/Tracker/README.html"><img src="images/tracker-rmd.png" width="500"/></a></center>
 
 
 ## Published Output Types <img src="images/publish.png" width="50"/>
@@ -11,19 +11,19 @@ You can create your content with these open source R packages like [R Markdown](
 
 #### Reports
 
-* [HTML Report](http://colorado.rstudio.com/rsc/tracker-report/tracker-report.html) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1095/access)</small>
-* [PDF Report](http://colorado.rstudio.com:3939/content/1109/tracker-pdf.pdf) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1109/access) </small>
-* [Word Report](http://colorado.rstudio.com:3939/content/1247/vcoWkF5eS/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1247/access/1432) </small>
+* [HTML Report](http://colorado.rstudio.com/rsc/tracker-report/tracker-report.html) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1609/access)</small>
+* [PDF Report](http://colorado.rstudio.com:3939/content/1644/tracker-pdf.pdf) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1644/access) </small>
+* [Word Report](http://colorado.rstudio.com:3939/content/1643/vcoWkF5eS/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1643/access/1432) </small>
 
 #### Presentations
 
-* [HTML Presentation](http://colorado.rstudio.com:3939/content/1110/tracker-ioslides.html) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1110/access/1229)</small>
-* [PowerPoint Presentation](http://colorado.rstudio.com:3939/content/1246/vMEHUS5Ks/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1246/access/1429)</small>
+* [HTML Presentation](http://colorado.rstudio.com:3939/content/1645/tracker-ioslides.html) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1645/access/1229)</small>
+* [PowerPoint Presentation](http://colorado.rstudio.com:3939/content/1646/vMEHUS5Ks/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1646/access/1429)</small>
 
 #### Interactive docs and apps
 
 * [Interactive Document](http://colorado.rstudio.com:3939/content/1121/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1121/access)</small>
-* [Flexdashboard](http://colorado.rstudio.com:3939/content/1094/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1094/access)</small>
+* [Flexdashboard](http://colorado.rstudio.com:3939/content/1642/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1642/access)</small>
 * [Shiny App](http://colorado.rstudio.com:3939/content/1120/) <small>[[login]](http://colorado.rstudio.com:3939/connect/#/apps/1120)</small>
 
 #### API's
--- output-formats/plumberapi/plumber.R
+++ output-formats/plumberapi/plumber.R
@@ -10,43 +10,46 @@ curr <- read_csv("data/trackerCalcCurr.csv")
 pre <- read_csv("data/trackerCalcPre.csv")
 users <- read_csv("data/users.csv")
 
-f <- function(x, y) {100 * (y / x - 1)}
+f <- function(x, y) {(y / x - 1)}
 
 dat <- function(seg, grp, per){
   bind_cols(
-    curr %>%
-      filter(segment == seg) %>%
-      select(ends_with(paste0(grp, per))) %>%
-      rename_at(1:3, ~c("purchasesCurr", "itemsCurr", "dollarsCurr")),
-    pre %>%
-      filter(segment == seg) %>%
-      select(ends_with(paste0(grp, per))) %>%
-      rename_at(1:3, ~c("purchasesPre", "itemsPre", "dollarsPre"))
+  curr %>%
+    filter(segment == seg) %>%
+    select(ends_with(paste0(grp, per))) %>%
+    rename_at(1:3, ~c("purchasesCurr", "itemsCurr", "dollarsCurr")),
+  pre %>%
+    filter(segment == seg) %>%
+    select(ends_with(paste0(grp, per))) %>%
+    rename_at(1:3, ~c("purchasesPre", "itemsPre", "dollarsPre"))
+) %>%
+  mutate(
+    Week = 1:52,
+    RevenuePre = dollarsPre,
+    RevenueCurr = dollarsCurr,
+    Revenue = f(dollarsPre, dollarsCurr),
+    CustomersPre = filter(users, segment == seg) %>% .$pre,
+    CustomersCurr = filter(users, segment == seg) %>% .$curr,
+    Customers = f(CustomersPre, CustomersCurr),
+    VisitsPre = purchasesPre / CustomersPre,
+    VisitsCurr = purchasesCurr / CustomersCurr,
+    Visits = f(VisitsPre, VisitsCurr),
+    ItemsPre = itemsPre / purchasesPre,
+    ItemsCurr = itemsCurr / purchasesCurr,
+    Items = f(ItemsPre, ItemsCurr),
+    SpendPre = dollarsPre / itemsPre,
+    SpendCurr = dollarsCurr / itemsCurr,
+    Spend = f(SpendPre, SpendCurr)
   ) %>%
-    mutate(
-      week = 1:52,
-      dollarsPct = f(dollarsPre, dollarsCurr),
-      usersPre = filter(users, segment == seg) %>% .$pre,
-      usersCurr = filter(users, segment == seg) %>% .$curr,
-      usersPct = f(usersPre, usersCurr),
-      purUserPre = purchasesPre / usersPre,
-      purUserCurr = purchasesCurr / usersCurr,
-      purUserPct = f(purUserPre, purUserCurr),
-      itemsPurPre = itemsPre / purchasesPre,
-      itemsPurCurr = itemsCurr / purchasesCurr,
-      itemsPurPct = f(itemsPurPre, itemsPurCurr),
-      dollItemsPre = dollarsPre / itemsPre,
-      dollItemsCurr = dollarsCurr / itemsCurr,
-      dollItemsPct = f(dollItemsPre, dollItemsCurr)
-    ) %>%
-    filter(week <= 22) %>%
-    select(
-      week, dollarsPre, dollarsCurr, dollarsPct,
-      usersPre, usersCurr, usersPct,
-      purUserPre, purUserCurr, purUserPct,
-      itemsPurPre, itemsPurCurr, itemsPurPct,
-      dollItemsPre, dollItemsCurr, dollItemsPct
-    )
+  filter(Week <= 22) %>%
+  #arrange(desc(Week)) %>%
+  select(
+    Week, RevenuePre, RevenueCurr, Revenue,
+    CustomersPre, CustomersCurr, Customers,
+    VisitsPre, VisitsCurr, Visits,
+    ItemsPre, ItemsCurr, Items,
+    SpendPre, SpendCurr, Spend
+  )
 }
 
 #* @get /data
@@ -55,8 +58,7 @@ dat <- function(seg, grp, per){
 #* @param seg Segment (Total, Heavy, Mainstream, Focus1, Focus2, Specialty, Diverse1, Diverse2, Other, New)
 out <- function(seg = "Total", grp = "Total", per = "Week"){
   dat(seg, grp, per) %>%
-    select(week, dollarsPct, usersPct, purUserPct, itemsPurPct, dollItemsPct) %>%
-    mutate_at(vars(dollarsPct:dollItemsPct), round, 2)
+    select(Week, RevenuePre, RevenueCurr, Revenue, Customers, Visits, Items, Spend)
 }
 
 #* @get /plot
@@ -65,19 +67,20 @@ out <- function(seg = "Total", grp = "Total", per = "Week"){
 #* @param seg Segment (Total, Heavy, Mainstream, Focus1, Focus2, Specialty, Diverse1, Diverse2, Other, New)
 #* @png
 plot <- function(seg = "Total", grp = "Total", per = "Week"){
-  
+
   pdat <- dat(seg, grp, per) %>%
-    select(week, dollarsPct, usersPct, purUserPct, itemsPurPct, dollItemsPct) %>%
-    gather(seg, metric, -week) %>%
-    mutate(metric = round(metric, 2))
+    select(Week, Revenue, Customers, Visits, Items, Spend) %>%
+    gather(seg, metric, -Week) %>%
+    mutate(metric = round(100 * metric, 2)) %>%
+    mutate(seg = factor(seg, levels = c("Spend", "Items", "Visits", "Customers", "Revenue")))
   
-  p1 <- ggplot(data = filter(pdat, seg != "dollarsPct"), aes(week, metric, fill = seg)) +
+  p1 <- ggplot(filter(pdat, seg != "Revenue"), aes(Week, metric, fill = seg)) +
     geom_bar(stat = "Identity") + 
-    geom_line(data = filter(pdat, seg == "dollarsPct"), aes(week, metric), col = "darkgrey") +
-    scale_fill_manual(values = alpha(c("darkgrey", "lightgreen", "salmon", "lightblue", "orange"), 0.5)) +
+    geom_line(data = filter(pdat, seg == "Revenue"), aes(Week, metric), col = "darkgrey") +
+    scale_fill_manual(values = alpha(c("orange", "salmon", "darkgrey", "lightgreen", "lightblue"), 0.5)) +
     labs(x = "Week", y = "Percent", title = "Percentage change by Week") +
-    theme_minimal()
-  
+    theme_minimal() +
+    theme(legend.title=element_blank())
+
   print(p1)
 }
-
